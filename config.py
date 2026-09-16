d75ea0a9cab1996f371b1b45144781fac6d0804f
wynex <qiwee617@gmail.com>
2026-09-16 22:39:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -16,49 +16,34 @@ DEFAULT_DATA = {
     "question_logs": {},
     "schedule": {
         "Pazartesi": [
-            {"time": "07:20 - 08:00", "lesson": "Türkçe", "teacher": "Büşra Özen"},
-            {"time": "08:10 - 08:50", "lesson": "Türkçe", "teacher": "Büşra Özen"},
-            {"time": "09:00 - 09:40", "lesson": "Matematik", "teacher": "Ahmet Yılmaz"},
-            {"time": "09:50 - 10:30", "lesson": "Matematik", "teacher": "Ahmet Yılmaz"},
-            {"time": "10:40 - 11:20", "lesson": "Fen Bilimleri", "teacher": "Mehmet Demir"},
-            {"time": "11:30 - 12:10", "lesson": "Fen Bilimleri", "teacher": "Mehmet Demir"},
-            {"time": "12:20 - 13:00", "lesson": "İngilizce", "teacher": "Ayşe Kaya"}
+            {"time": "07:20 - 08:50", "lesson": "İnkılap", "teacher": "Ayşenur Temel"},
+            {"time": "09:00 - 10:30", "lesson": "Görgütü-8", "teacher": "İlknur Kırmızı"},
+            {"time": "10:40 - 12:10", "lesson": "Matematik", "teacher": "Rumeysa Kan"},
+            {"time": "12:20 - 13:00", "lesson": "Fen Bilimleri", "teacher": "Şeymanur Güner"}
         ],
         "Salı": [
-            {"time": "07:20 - 08:00", "lesson": "Matematik", "teacher": "Ahmet Yılmaz"},
-            {"time": "08:10 - 08:50", "lesson": "Matematik", "teacher": "Ahmet Yılmaz"},
-            {"time": "09:00 - 09:40", "lesson": "Türkçe", "teacher": "Büşra Özen"},
-            {"time": "09:50 - 10:30", "lesson": "İnkılap Tarihi", "teacher": "Ayşenur Temel"},
-            {"time": "10:40 - 11:20", "lesson": "İnkılap Tarihi", "teacher": "Ayşenur Temel"},
-            {"time": "11:30 - 12:10", "lesson": "Din Kültürü", "teacher": "Hatice Akpınar"},
-            {"time": "12:20 - 13:00", "lesson": "Din Kültürü", "teacher": "Hatice Akpınar"}
+            {"time": "07:20 - 08:50", "lesson": "Türkçe 2", "teacher": "Büşra Özen"},
+            {"time": "09:00 - 10:30", "lesson": "Matematik", "teacher": "Rumeysa Kan"},
+            {"time": "10:40 - 12:10", "lesson": "Fen Bilimleri", "teacher": "Şeymanur Güner"},
+            {"time": "12:20 - 13:00", "lesson": "Beden Eğitimi", "teacher": "Uğur Yağız Bozkurt"}
         ],
         "Çarşamba": [
-            {"time": "07:20 - 08:00", "lesson": "Türkçe", "teacher": "Büşra Özen"},
-            {"time": "08:10 - 08:50", "lesson": "Türkçe", "teacher": "Büşra Özen"},
+            {"time": "07:20 - 08:50", "lesson": "Türkçe 2", "teacher": "Büşra Özen"},
             {"time": "09:00 - 09:40", "lesson": "Rehberlik", "teacher": "Şeymanur Güner"},
-            {"time": "09:50 - 10:30", "lesson": "Türk Sosyal Hayatı", "teacher": "Ayşenur Temel"},
-            {"time": "10:40 - 11:20", "lesson": "Din Kültürü", "teacher": "Hatice Akpınar"},
-            {"time": "11:30 - 12:10", "lesson": "Seçmeli Matematik", "teacher": "Ahmet Yılmaz"},
-            {"time": "12:20 - 13:00", "lesson": "Seçmeli Matematik", "teacher": "Ahmet Yılmaz"}
+            {"time": "09:50 - 11:20", "lesson": "Türk Sosyal H.A", "teacher": "Ayşenur Temel"},
+            {"time": "11:30 - 13:00", "lesson": "Din Kültürü", "teacher": "Hatice Akpınar"}
         ],
         "Perşembe": [
-            {"time": "07:20 - 08:00", "lesson": "Fen Bilimleri", "teacher": "Mehmet Demir"},
-            {"time": "08:10 - 08:50", "lesson": "Fen Bilimleri", "teacher": "Mehmet Demir"},
-            {"time": "09:00 - 09:40", "lesson": "Matematik", "teacher": "Ahmet Yılmaz"},
-            {"time": "09:50 - 10:30", "lesson": "İngilizce", "teacher": "Ayşe Kaya"},
-            {"time": "10:40 - 11:20", "lesson": "İngilizce", "teacher": "Ayşe Kaya"},
-            {"time": "11:30 - 12:10", "lesson": "Görsel Sanatlar", "teacher": "Zeynep Can"},
-            {"time": "12:20 - 13:00", "lesson": "Müzik", "teacher": "Ali Şen"}
+            {"time": "07:20 - 08:50", "lesson": "İngilizce 2", "teacher": "Kevser Temel"},
+            {"time": "09:00 - 10:30", "lesson": "Türkçe 2", "teacher": "Büşra Özen"},
+            {"time": "10:40 - 11:20", "lesson": "Müzik", "teacher": "İlknur Kırmızı"},
+            {"time": "11:30 - 13:00", "lesson": "Peyg. Hayatı-8", "teacher": "Hatice Akpınar"}
         ],
         "Cuma": [
-            {"time": "07:20 - 08:00", "lesson": "İngilizce", "teacher": "Ayşe Kaya"},
-            {"time": "08:10 - 08:50", "lesson": "İngilizce", "teacher": "Ayşe Kaya"},
-            {"time": "09:00 - 09:40", "lesson": "Matematik", "teacher": "Ahmet Yılmaz"},
-            {"time": "09:50 - 10:30", "lesson": "Fen Bilimleri", "teacher": "Mehmet Demir"},
-            {"time": "10:40 - 11:20", "lesson": "Beden Eğitimi", "teacher": "Murat Polat"},
-            {"time": "11:30 - 12:10", "lesson": "Beden Eğitimi", "teacher": "Murat Polat"},
-            {"time": "12:20 - 13:00", "lesson": "Teknoloji Tasarım", "teacher": "Sibel Arslan"}
+            {"time": "07:20 - 08:50", "lesson": "İngilizce 2", "teacher": "Kevser Temel"},
+            {"time": "09:00 - 10:30", "lesson": "Teknoloji Tasarım", "teacher": "İlker Mermer"},
+            {"time": "10:40 - 12:10", "lesson": "Matematik", "teacher": "Rumeysa Kan"},
+            {"time": "12:20 - 13:00", "lesson": "Görsel", "teacher": "Semahat Yabalak"}
         ]
     }
 }
