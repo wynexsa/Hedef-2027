4f2a64dbde5868a7f9f3a1cfb93425304246a76c
wynex <qiwee617@gmail.com>
2026-09-16 23:15:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,12 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_FILE = os.path.join(BASE_DIR, "lgs_data.json")
 
-# Şifreler
+# Sistem Admin Şifresi
 PASSWORD_ADMIN = "W1ndows_LinuX#2026"
-PASSWORD_TEACHER = "OrucReis_8C!"
 
 DEFAULT_DATA = {
-    "student": {
-        "full_name": "",
-        "school_number": "",
-        "password": "",
-        "secret_question": "",
-        "secret_answer": "",
-        "notifications": []  
-    },
+    "students": [],      # Birden fazla öğrencinin tutulacağı liste
+    "teachers": [],      # Kayıt olan öğretmenlerin tutulacağı liste
     "announcements": [],
     "question_logs": {}
 }
